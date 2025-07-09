72bd0e9a6faa5adcf94c9aee1b18e082d69aa974
juanesgl <142745887+juanesgl@users.noreply.github.com>
2025-07-08 21:34:46 -0500

    New thingyssssssssssssssssss

---
+++ Trainings/Training #3 Programacion dinamica/C++/Juanes/hi.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello World! 👋\n");
+    printf("Systems Engineer 👩‍💻\n");
+    return 0;
+}
+
+
+
+
---
+++ Trainings/Training #3 Programacion dinamica/Java/Juanes/collatz.java
@@ -0,0 +1,19 @@
+package Juanes;
+
+
+import java.util.Map;
+
+public class collatz {
+
+    map<Integer, Integer> memo = new Map<>();
+    public int collaz(int n){
+        if (n == 1){
+            return 1;
+        }
+    }
+    public static void main(String[] args){
+
+
+
+    }
+}
